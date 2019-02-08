f8a6d8927e089b738fe5ce58fe1bfafb13748d8c
galloaleonardo <galloaleonardo@gmail.com>
2019-02-08 00:13:31 -0200

    Pacient Change

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
-from persons import Gerontologian
+from persons import Gerontologian, Patient
 from utils.headers import Headers
+import time
 
 
 class StartApplication:
@@ -7,17 +8,39 @@ class StartApplication:
     def init_app():
         Headers.title_header()
         GerontologianPeople.get_gerontologian()
+        print('Vamos iniciar com o cadastro do paciente... \n')
+        time.sleep(3)
+        PatientPeople.get_patient()
 
 
 class GerontologianPeople:
     @staticmethod
     def get_gerontologian():
-        name = input('Olá gerontologo(a), qual o seu nome? ')
-        abg = input('Certo, e qual é o número do seu cadastro na ABG? ')
-        confirm = input('Nome: %s | Num. ABG: %s. Podemos confirmar o cadastro (Y/N)? ' % (name, abg))
-        if confirm.upper() == 'Y':
-            geronto = Gerontologian()
-            geronto.set_gerontologian(name, abg)
-            print('Bem vindo(a), %s.' % geronto.geronto_name)
+        name = input('Olá gerontologo(a), digite o seu nome: ')
+        abg = input('%s, qual seu númedo da ABG? ' % name)
+        confirm = input('Nome: %s | Num. ABG: %s. Podemos confirmar o cadastro (S / N)? ' % (name, abg))
+        if confirm.upper() == 'S':
+            gerontologian = Gerontologian(name, abg)
+            print('Bem vindo(a), %s.' % gerontologian.g_name)
         else:
             pass
+
+
+class PatientPeople:
+    @staticmethod
+    def get_patient():
+        name = input('Nome: ')
+        birth = input('Data de nascimento: ')
+        sex = input('Sexo - Feminino (F) | Masculino (M) | Prefiro não dizer (O): ')
+        marital_status = input('Estado civíl: ')
+        schooling = input('Escolaridade: ')
+        ocupation = input('Ocupação: ')
+        home_with = input('Com quem mora: ')
+        income = input('Renda individual ou familiar: ')
+        bpc = input('BPC - Sim (S) | Não (N) | Não se aplica (O): ')
+        b_religion = input('Possui religião - Sim (S) | Não (N):')
+        religion_name = input('Se sim, qual: ')
+
+        patient = Patient(name, birth, sex, marital_status, schooling, ocupation, home_with,
+                          income, bpc, b_religion, religion_name)
+        confirm = input('Ficha de %s, confirmar? (S / N)' % patient.p_name)
--- persons.py
+++ persons.py
@@ -1,8 +1,20 @@
-class Gerontologian():
-    def __init__(self):
-        self.geronto_name = ''
-        self.geronto_num_abg = ''
+class Gerontologian:
+    def __init__(self, name, num_abg):
+        self.g_name = name
+        self.g_num_abg = num_abg
 
-    def set_gerontologian(self, name, num_abg):
-        self.geronto_name = name
-        self.geronto_num_abg = num_abg
+
+class Patient:
+    def __init__(self, name, birth, sex, marital_status, schooling, ocupation,
+                 home_with, income, bpc, b_religion, religion_name):
+        self.p_name = name
+        self.p_birth = birth
+        self.p_sex = sex
+        self.p_marital_status = marital_status
+        self.p_schooling = schooling
+        self.p_ocupation = ocupation
+        self.p_home_with = home_with
+        self.p_income = income
+        self.p_bpc = bpc
+        self.p_religion = b_religion
+        self.p_religion_name = religion_name
